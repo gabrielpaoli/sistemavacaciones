d5402c0a13ff6703264e2b372052050fa002013d
Gabriel <gabrielpaoli@hotmail.com>
2017-09-14 00:28:16 -0300

    avance modulo vacaciones

---
+++ sites/all/modules/custom/sistemavacaciones/js/sistemavacaciones.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function() {
+	/*
+		var date = jQuery('.date-clear');
+
+		date.datepicker().on("input change", function (e) {
+	    console.log(e.target.value);
+		});
+
+
+	jQuery("#user-profile-form #edit-submit").click(function(event){
+		if(jQuery('.form-item-field-dias-restantes-de-vacacion-und-0-value input').val() < 0){
+			event.preventDefault()
+			alert('Los días restantes de vacaciones no pueden ser menores a 0');
+		}
+	});
+
+	*/
+
+});
